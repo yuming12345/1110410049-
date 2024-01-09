e3e16db7c79bd1ab46233f59f3bda2bc75ac7a78
yuming12345 <95611008+yuming12345@users.noreply.github.com>
2024-01-09 16:54:17 +0800

    Add files via upload

---
+++ Finalterm/src/StarWar_1110410049/Main.java
@@ -0,0 +1,12 @@
+package StarWar_1110410049;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		 
+
+	}
+
+	
+}
---
+++ Finalterm/src/StarWar_1110410049/XWing.java
@@ -0,0 +1,83 @@
+package StarWar_1110410049;
+
+import java.util.Arrays;
+
+public class XWing {
+	String Model;
+	double Length;
+	double AirSpeed;
+	String SpaceSpeed;
+	String StdandardEngine;
+	String HyperSpaceEngine;
+	String[] Weapon;
+	double Weapon_power;
+	
+	public XWing(String Model,double Length,double AirSpeed,
+			String SpaceSpeed,String StdandardEngine,String HyperSpaceEngine,String[] Weapon,double Weapon_power){
+		this.Model=Model;
+		this.Length=Length;
+		this.AirSpeed=AirSpeed;
+		this.SpaceSpeed=SpaceSpeed;
+		this.StdandardEngine=StdandardEngine;
+		this.HyperSpaceEngine=HyperSpaceEngine;
+		this.Weapon=Weapon;
+		this.Weapon_power=Weapon_power;
+}
+	
+	@Override
+	public String toString() {
+		return "XWing [Model=" + Model + ", Length=" + Length + ", AirSpeed=" + AirSpeed + ", SpaceSpeed=" + SpaceSpeed
+				+ ", StdandardEngine=" + StdandardEngine + ", HyperSpaceEngine=" + HyperSpaceEngine + ", Weapon="
+				+ Arrays.toString(Weapon) + ", Weapon_power=" + Weapon_power + "]";
+	}
+
+	public String getModel() {
+		return Model;
+	}
+	public void setModel(String model) {
+		Model = model;
+	}
+	public double getLength() {
+		return Length;
+	}
+	public void setLength(double length) {
+		Length = length;
+	}
+	public double getAirSpeed() {
+		return AirSpeed;
+	}
+	public void setAirSpeed(double airSpeed) {
+		AirSpeed = airSpeed;
+	}
+	public String getSpaceSpeed() {
+		return SpaceSpeed;
+	}
+	public void setSpaceSpeed(String spaceSpeed) {
+		SpaceSpeed = spaceSpeed;
+	}
+	public String getStdandardEngine() {
+		return StdandardEngine;
+	}
+	public void setStdandardEngine(String stdandardEngine) {
+		StdandardEngine = stdandardEngine;
+	}
+	public String getHyperSpaceEngine() {
+		return HyperSpaceEngine;
+	}
+	public void setHyperSpaceEngine(String hyperSpaceEngine) {
+		HyperSpaceEngine = hyperSpaceEngine;
+	}
+	public String[] getWeapon() {
+		return Weapon;
+	}
+	public void setWeapon(String[] weapon) {
+		Weapon = weapon;
+	}
+	public double getWeapon_power() {
+		return Weapon_power;
+	}
+	public void setWeapon_power(double weapon_power) {
+		Weapon_power = weapon_power;
+	}
+	
+}
